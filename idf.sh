c8532b92dabf8030ad08c7b5f0b3fc794881401d
Sascha Häberling <mail@sascha-haeberling.com>
2022-03-27 12:58:57 -0700

    Make idf.sh use parameters.

--- idf.sh
+++ idf.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-idf.py -p /dev/ttyS3 -b 921600 "$1"
+if [ -z "$1" ]
+  then
+    echo "1st argument missing: [serial-port number]"
+    exit 1
+fi
+if [ -z "$2" ]
+  then
+    echo "2nd argument missing: [command]"
+    exit 1
+fi
+
+
+idf.py -p /dev/ttyS"$1" -b 921600 "$2"
